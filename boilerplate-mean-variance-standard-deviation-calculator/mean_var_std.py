2d49ff1b1e6210291ac66c13ed19a05facc651d4
nbayer2 <77975521+nbayer2@users.noreply.github.com>
2022-01-05 08:12:06 +0100

    Add files via upload

---
+++ boilerplate-mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def calculate(list):
+  if len(list)!=9:
+    raise ValueError("List must contain nine numbers.")
+    
+  x=np.array(list)
+  x=x.reshape(3,3)
+  #print(x)
+  calculations={'mean':[x.mean(axis=0).tolist(),x.mean(axis=1).tolist(),x.mean()],
+                'variance':[x.var(axis=0).tolist(),x.var(axis=1).tolist(),x.var()],
+                'standard deviation':[x.std(axis=0).tolist(),x.std(axis=1).tolist(),x.std()],
+                'max':[x.max(axis=0).tolist(),x.max(axis=1).tolist(),x.max()],
+                'min':[x.min(axis=0).tolist(),x.min(axis=1).tolist(),x.min()],
+                'sum':[x.sum(axis=0).tolist(),x.sum(axis=1).tolist(),x.sum()]
+                }
+  print(calculations)
+  return calculations
